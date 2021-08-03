b5ee0d2a490a79df1074e415c385b7f837db4dfa
Max Geng <leegxf@gmail.com>
2021-08-03 22:18:51 +0800

    添加项目文件。

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ItemsSourceBindEnum.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "ComBox ItemsSource bind enum";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public ObservableCollection<Week> Weeks
+        {
+            get
+            {
+                return weeks;
+            }
+        }
+
+        private Week selectWeek;
+
+        public Week SelectWeek
+        {
+            get { return selectWeek; }
+            set 
+            {
+                SetProperty(ref selectWeek, value);
+            }
+        }
+
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        ObservableCollection<Week> weeks = new ObservableCollection<Week>(Enum.GetValues(typeof(Week)).Cast<Week>());
+    }
+
+    public enum Week
+    {
+        Monday=0,
+        Tuesday=1,
+        Wednesday=2,
+        Thursday=3,
+        Friday=4,
+        Saturday=5,
+        Sunday=6
+    }
+}
